621378601c22f5096bba23af88efaf6a7e7c1ffc
vadimborisov <malabarsik2@mail.ru>
2023-02-08 14:27:05 +0300

    Added CBV ProductsListViews in products/views.py; deleted FBV products; changed paths category, paginator in products/urls.py; changed pagination in products.html for using with ProductsListViews

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import products, basket_add, basket_remove
+from .views import ProductsListViews, basket_add, basket_remove
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('category/<int:category_id>/', products, name='category'),
-    path('page/<int:page_number>/', products, name='paginator'),
+    path('', ProductsListViews.as_view(), name='index'),
+    path('category/<int:category_id>/', ProductsListViews.as_view(),
+         name='category'),
+    path('page/<int:page>/', ProductsListViews.as_view(),
+         name='paginator'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove,
          name='basket_remove'),
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from .models import ProductCategory, Product, Basket
 
@@ -9,6 +10,8 @@ PER_PAGE = 3
 
 
 class IndexView(TemplateView):
+    """CBV для отображения шаблона домашней страницы"""
+
     template_name = 'products/index.html'
 
     def get_context_data(self, **kwargs):
@@ -23,24 +26,51 @@ class IndexView(TemplateView):
 #     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None, page_number=1):
-    """
-    Контроллер для отображения всех продуктов или для отображения
-    товаров определенных отфильтрованных по категории продуктов
-    """
+class ProductsListViews(ListView):
+    """CBV для отображения шаблона со списком всех доступных продуктов"""
 
-    products = Product.objects.filter(
-        category_id=category_id) if category_id else Product.objects.all()
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = PER_PAGE
 
-    paginator = Paginator(products, PER_PAGE)
-    products_paginator = paginator.page(page_number)
+    def get_queryset(self):
+        """Переопределяем метод. Если category_id == None, возвращает
+        весь queryset; если category_id != None, возвращает queryset
+        продуктов, отфильтрованных по переданной категории
+        """
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')  # Достаем category_id из
+        # словаря со всеми данными. Если category_id нет, get() вернет None
+        if category_id:
+            return queryset.filter(category_id=category_id)
+        else:
+            return queryset
 
-    context = {
-        'title': 'Store - Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator
-    }
-    return render(request, 'products/products.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+
+# def products(request, category_id=None, page_number=1):
+#     """
+#     Контроллер для отображения всех продуктов или для отображения
+#     товаров определенных отфильтрованных по категории продуктов
+#     """
+#
+#     products = Product.objects.filter(
+#         category_id=category_id) if category_id else Product.objects.all()
+#
+#     paginator = Paginator(products, PER_PAGE)
+#     products_paginator = paginator.page(page_number)
+#
+#     context = {
+#         'title': 'Store - Каталог',
+#         'categories': ProductCategory.objects.all(),
+#         'products': products_paginator
+#     }
+#     return render(request, 'products/products.html', context)
 
 
 @login_required
